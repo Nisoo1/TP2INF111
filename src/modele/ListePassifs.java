f9afd22e56d1f82f315e3499b4d88d8382fc3ef7
Nisoo1 <iguebg@gmail.com>
2026-07-17 01:01:47 -0400

    requis 1-4

--- src/modele/ListePassifs.java
+++ src/modele/ListePassifs.java
@@ -1,4 +1,23 @@
 package modele;
 
+import java.util.ArrayList;
+
+//requis 1, 2, 3 et 4 pour le moment
+
 public class ListePassifs {
+
+    private ArrayList<Passif> passifs;
+
+    private class Passif {
+        private String id;
+        private String description;
+        private double solde;
+
+
+        public Passif(String id, String description, double solde) {
+            this.id = id;
+            this.description = description;
+            this.solde = solde;
+        }
+    }
 }
